0a29318931777eb7db8dde58acc44dca0a517eff
mljunker <maju1971@colorado.edu>
2018-03-28 19:45:46 -0600

    Package practice, doesn't seem to create anything executable

---
+++ simple_package.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+package body Simple_Package is
+	procedure Hello is
+	begin
+		Ada.Text_IO.Put_Line ("Hello");
+	end Hello;
+end Simple_Package;
---
+++ simple_package.ads
@@ -0,0 +1,4 @@
+with Ada.Text_IO;
+package Simple_Package is
+	procedure Hello;
+end Simple_Package;
